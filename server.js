4b7a12ececce23c418dad38c977d756c9e3b2ed0
geordyp <geowilliams6@gmail.com>
2017-01-26 14:20:30 -0600

    Complete in class work

--- server.js
+++ server.js
@@ -1,6 +1,53 @@
+"use strict";
+
 /**
  * server.js
  * This file defines the server for a
  * simple photo gallery web app.
  */
- 
+var http = require('http');
+var fs = require('fs');
+var port = 3000;
+
+function serveImage(filename, req, res) {
+  fs.readFile('images/' + filename, function(err, body) {
+    if (err) {
+      console.error(err);
+      res.statusCode == 500;
+      res.statusMessage = "Whoops";
+      return;
+    }
+    res.setHeader("Content-Type", "image/jpeg");
+    res.end(body);
+  });
+}
+
+var server = http.createServer((req, res) => {
+
+  switch (req.url) {
+    case "/ace":
+      serveImage('ace.jpg', req, res);
+      break;
+    case "/bubble":
+      serveImage('bubble.jpg', req, res);
+      break;
+    case "/chess":
+      serveImage('chess.jpg', req, res);
+      break;
+    case "/fern":
+      serveImage('fern.jpg', req, res);
+      break;
+    case "/mobile":
+      serveImage('mobile.jpg', req, res);
+      break;
+    default:
+      res.statusCode = 404;
+      res.statusMessage = "Not found";
+      res.end();
+  }
+
+});
+
+server.listen(port, () => {
+  console.log("Listening on Port " + port);
+});
