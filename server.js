9d3cb7d5e6d5ab18ca90ac58f55c4539a907f274
geordyp <geowilliams6@gmail.com>
2017-01-31 14:21:22 -0600

    Complete in class work for 01-31

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ var http = require('http');
 var fs = require('fs');
 var port = 3000;
 
+var stylesheet = fs.readFileSync('gallery.css');
+
+var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
+
 function serveImage(filename, req, res) {
   fs.readFile('images/' + filename, function(err, body) {
     if (err) {
@@ -25,25 +29,50 @@ function serveImage(filename, req, res) {
 var server = http.createServer((req, res) => {
 
   switch (req.url) {
+    case "/gallery":
+      var gHtml = imageNames.map(function(fileName) {
+        return '<img src="' + fileName + '">';
+      }).join(' ');
+      var html =  '<!DOCTYPE html>';
+          html += '<head>';
+          html += '<title>Dynamic Page</title>';
+          html += '<link rel="stylesheet" type="text/css" href="gallery.css">';
+          html += '</head>';
+          html += '<body>';
+            html += '<h1>Gallery</h1>';
+            html += gHtml;
+          html += '</body>';
+      res.setHeader("Content-Type", "text/html");
+      res.end(html);
+      break;
     case "/ace":
+    case "/ace.jpg":
       serveImage('ace.jpg', req, res);
       break;
     case "/bubble":
+    case "/bubble.jpg":
       serveImage('bubble.jpg', req, res);
       break;
     case "/chess":
+    case "/chess.jpg":
       serveImage('chess.jpg', req, res);
       break;
     case "/fern":
+    case "/fern.jpg":
       serveImage('fern.jpg', req, res);
       break;
     case "/mobile":
+    case "/mobile.jpg":
       serveImage('mobile.jpg', req, res);
+      break;
+    case "/gallery.css":
+      res.setHeader('Content-Type', 'text/css');
+      res.end(stylesheet);
       break;
     default:
       res.statusCode = 404;
-      res.statusMessage = "Not found";
-      res.end();
+      res.statusMessage = "Resource not found";
+      res.end("Couldn't find it");
   }
 
 });
