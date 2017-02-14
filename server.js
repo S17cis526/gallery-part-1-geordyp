6ec3d50396c3173d18f3b9b22fe9a036d810b7d7
geordyp <geowilliams6@gmail.com>
2017-02-14 13:45:13 -0600

    Complete in class work from 02-09

--- server.js
+++ server.js
@@ -5,13 +5,17 @@
  * This file defines the server for a
  * simple photo gallery web app.
  */
+var multipart = require('./multipart');
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var port = 3000;
+
+/* load cached files */
 var config = JSON.parse(fs.readFileSync('config.json'));
 var stylesheet = fs.readFileSync('gallery.css');
-var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
+
+// var imageNames = ['ace.jpg', 'bubble.jpg', 'chess.jpg', 'fern.jpg', 'mobile.jpg'];
 
 function getImageNames(callback) {
   fs.readdir('images/', function(err, fileNames) {
@@ -23,45 +27,6 @@ function getImageNames(callback) {
 function imageNamesToTags(fileNames) {
   return fileNames.map(function(fileName) {
     return `<img src="${fileName}" alt="${fileName}">`;
-  });
-}
-
-function serveImage(filename, req, res) {
-  fs.readFile('images/' + filename, function(err, body) {
-    if (err) {
-      console.error(err);
-      res.statusCode == 500;
-      res.statusMessage = "Whoops";
-      return;
-    }
-    res.setHeader("Content-Type", "image/jpeg");
-    res.end(body);
-  });
-}
-
-function uploadImage(req, res) {
-  var body = '';
-
-  req.on('error', function() {
-    res.statusCode = 500;
-    res.end();
-  });
-
-  req.on('data', function(data) {
-    body += data;
-  });
-
-  req.on('end', function() {
-    fs.writeFile('filename', body, function(err) {
-      if (err) {
-        console.error(err);
-        res.statusCode = 500;
-        res.end();
-        return;
-      }
-
-      serveGallery(req, res);
-    });
   });
 }
 
@@ -101,36 +66,77 @@ function serveGallery(req, res) {
   })
 }
 
-var server = http.createServer((req, res) => {
+function serveImage(filename, req, res) {
+  fs.readFile('images/' + decodeURIComponent(filename), function(err, data) {
+    if (err) {
+      console.error(err);
+      res.statusCode == 404;
+      res.statusMessage = "Resource not found";
+      res.end()
+      return;
+    }
+    res.setHeader("Content-Type", "image/*");
+    res.end(data);
+  });
+}
+
+function uploadImage(req, res) {
+  multipart(req, res, function(req, res) {
+    // make sure an image was uploaded
+    console.log('filename', req.body.filename)
+    if(!req.body.image.filename) {
+      console.error("No file in upload");
+      res.statusCode = 400;
+      res.statusMessage = "No file specified"
+      res.end("No file specified");
+      return;
+    }
+    fs.writeFile('images/' + req.body.image.filename, req.body.image.data, function(err){
+      if(err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.statusMessage = "Server Error";
+        res.end("Server Error");
+        return;
+      }
+      serveGallery(req, res);
+    });
+  });
+}
+
+function handleRequest(req, res) {
+  // at most, the url should have two parts -
+  // a resource and a querystring separated by a ?
   var urlParts = url.parse(req.url);
 
-  if (urlParts.query) {
+  if(urlParts.query){
     var matches = /title=(.+)($|&)/.exec(urlParts.query);
-    if (matches && matches[1]) {
+    if(matches && matches[1]){
       config.title = decodeURIComponent(matches[1]);
       fs.writeFile('config.json', JSON.stringify(config));
     }
   }
 
-  switch (urlParts.pathname) {
-    case "/":
-    case "/gallery":
-      if (req.method == 'GET') {
+  switch(urlParts.pathname) {
+    case '/':
+    case '/gallery':
+      if(req.method == 'GET') {
         serveGallery(req, res);
-      } else if (req.method == 'POST') {
-        uploadPicture(req, res);
+      } else if(req.method == 'POST') {
+        uploadImage(req, res);
       }
       break;
-    case "/gallery.css":
+    case '/gallery.css':
       res.setHeader('Content-Type', 'text/css');
       res.end(stylesheet);
       break;
     default:
       serveImage(req.url, req, res);
   }
+}
 
-});
-
-server.listen(port, () => {
-  console.log("Listening on Port " + port);
+/* Create and launch the webserver */
+var server = http.createServer(handleRequest);
+server.listen(port, function(){
+  console.log("Server is listening on port ", port);
 });
