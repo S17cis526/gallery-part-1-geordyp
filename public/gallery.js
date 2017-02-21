5710a18bfc877a3db2b2cdc29b6d0078010195d2
geordyp <geowilliams6@gmail.com>
2017-02-21 14:17:14 -0600

    Complete in class work for 02-21

---
+++ public/gallery.js
@@ -0,0 +1,6 @@
+var title = document.getElementById('gallery-title');
+title.onclick = function(e) {
+  e.preventDefault();
+  var form = document.getElementById('gallery-title-edit');
+  form.style.display = 'inline';
+}
